9b8bf15b1e60485cfcfc34af00b143ce54e19280
Michael Tipton <mike@ibeta.org>
2021-10-07 14:50:46 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.set_capability('unhandledPromptBehavior', 'ignore')
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+
+driver = webdriver.Chrome(options=chrome_options)
+
+start_url = f"https://redhat.com"
+driver.get(start_url)
+
+print(driver.page_source)
